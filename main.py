e1499fccc0ecdcec1eb321e076bfe1486f78e61c
lucasaguiar-la <lucas.aguiar.ads@gmail.com>
2025-05-21 10:33:00 -0300

    feat: Structuring initial workflow

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+from time import sleep
 from scrapper.selenium_scrapper import extractor
+import json 
 
-run = extractor()
-print('\nSessão finalizada!')
+def workflow():
+    print('Sessão iniciada!')
+    job_vacancy = extractor()
+
+    if job_vacancy:
+        print(f'\nObjeto criado com sucesso:\n{json.dumps(job_vacancy, ensure_ascii=False, indent=4)}')
+
+if __name__ == '__main__':
+    try:
+        print('\nIniciando sessão...')
+        sleep(1)
+        workflow()
+    except Exception as e:
+        print(f'Algo deu errado ao executar o fluxo da operação: {e}')
+        raise
+    finally:
+        print('\nSessão finalizada!')
--- scrapper/selenium_scrapper.py
+++ scrapper/selenium_scrapper.py
@@ -1,4 +1,5 @@
 from time import sleep
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -14,25 +15,21 @@ from config.scrapper_config import(
     DATETIME_CLASS
     )
 
-import json 
-
 def extractor():
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--enable-unsafe-swiftshader')
-    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
 
-    print('Iniciando sessão...')
+    print('\nIniciando extração...')
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(url=LINKEDIN_URL)
-    print(driver)
 
     vacacy_link = driver.find_element(by=By.CSS_SELECTOR, value=LINK_CLASS)
     link = vacacy_link.get_attribute('href')
-    print(f'\nLink da vaga: {link}\n')
 
     try:
         driver.get(link)
@@ -41,19 +38,23 @@ def extractor():
         vacacy_title = driver.find_element(by=By.CSS_SELECTOR, value=TITLE_CLASS).text
         vacacy_company = driver.find_element(by=By.CSS_SELECTOR, value=COMPANY_CLASS).text
         vacacy_datetime = driver.find_element(by=By.CSS_SELECTOR, value=DATETIME_CLASS).text
-
-        print(
-            f'Vaga: {vacacy_title}\n'
-            f'Empresa: {vacacy_company}\n'
-            f'Postado {vacacy_datetime}\n'
-            )
+        request_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         
         vacacy_description = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, DESCRIPTION_CLASS)))
         vacacy_description_html = vacacy_description.get_attribute('innerHTML')
 
-        print(f'Descrição da vaga: {vacacy_description_html}')
-        return True
+        job_object = {
+            'Título': vacacy_title,
+            'Empresa': vacacy_company,
+            'Postado': vacacy_datetime,
+            'Descrição': vacacy_description_html,
+            'Link': link,
+            'Data da requisição': request_timestamp
+        }
+
+        return job_object
     except Exception as e:
-        return f'Algo deu errado na requisição: {e}'
+        print(f'Algo deu errado na requisição: {e}')
+        return False
     finally:
         driver.quit()
